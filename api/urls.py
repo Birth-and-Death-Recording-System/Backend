3600b5efb1a04ee3569849087521e131a9a829d2
PrinceOforiAnkamah <sabastainofori@gmail.com>
2024-02-15 07:46:32 +0000

    Fixed login code

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from api import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_user, name='login'),
     path('profile/', views.user_profile, name='profile'),
 ]
--- api/views.py
+++ api/views.py
@@ -7,15 +7,17 @@ from api.models import User
 from .serializers import UserSerializer, ProfileSerializer
 from rest_framework.authtoken.models import Token
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import login as auth_login
 
 
 # Create your views here.
 @api_view(['POST'])
-def login(request):
+def login_user(request):
     user = get_object_or_404(User, username=request.data['username'])
     if not user.check_password(request.data['password']):
         return Response({"detail": "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
     token, created = Token.objects.get_or_create(user=user)
+    auth_login(request, user)
     serializer = UserSerializer(instance=user)
     return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
